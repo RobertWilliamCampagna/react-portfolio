10ab7ed82d2483d051b07e959cb07b2ad8482fc6
Robert Campagna <rwcnyla@gmail.com>
2021-03-14 22:21:46 -0700

    spelling correction

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -8,7 +8,7 @@ function About() {
   <h1 id="about">Who am I?</h1>
   <img src={coverImage} className="mypic" alt="cover" />
   <p>
-    Robert Campagna from Long Islang, New York.  Father of twin boys Oliver and Wesley.  Born entertainer with the love for baseball.  Cancer survivor.  I am competetive. I live life by this rule, "There are alays two people for a job, you and the other guy.  F--K the other guy."
+    Robert Campagna from Long Islang, New York.  Father of twin boys Oliver and Wesley.  Born entertainer with the love for baseball.  Cancer survivor.  I am competetive. I live life by this rule, "There are always two people for a job, you and the other guy.  F--K the other guy."
   </p>
   </div>
 </section>
