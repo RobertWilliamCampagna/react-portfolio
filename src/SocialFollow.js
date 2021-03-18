17b5fdfd9334f4fa1d58dd4adbb8b4bb402fca8c
Robert Campagna <rwcnyla@gmail.com>
2021-03-17 18:55:51 -0700

    added logos to sticky footer/linkedin and instagram

--- src/SocialFollow.js
+++ src/SocialFollow.js
@@ -1,19 +1,27 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-    faGithub,
+    faGithub, faLinkedin, faInstagram
   } from "@fortawesome/free-brands-svg-icons";
 
 
 export default function SocialFollow() {
   return (
       <div className="social-container">
-      <h3>Social Follow</h3>
-      <a href="https://github.com/"
+      <a href="https://github.com/RobertWilliamCampagna"
         className="github-icon">
-        <FontAwesomeIcon icon={faGithub} size="2x" />
+        <FontAwesomeIcon icon={ faGithub } size="2x" />
+      </a>
+      <a href="https://www.linkedin.com/in/robert-campagna-5a11181b7/"
+        className="linkedin-icon">
+          <FontAwesomeIcon icon={ faLinkedin } size="2x" />
+      </a>
+      <a href="https://www.instagram.com/campagna.robert/"
+        className="instagram">
+          <FontAwesomeIcon icon={ faInstagram } size="2x" />
       </a>
 </div>
+
   
   );
 }
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,11 +6,14 @@ function About() {
   return (
     <section>
       <div className="about">
-  <h1 id="about">Who am I?</h1>
+  <h1 id="about">Lets Rock</h1>
   <img src={coverImage} className="mypic" alt="cover" />
   <p>
-    Robert Campagna from Long Islang, New York.  Father of twin boys Oliver and Wesley.  Born entertainer with the love for baseball and hockey.  Cancer survivor.  I am competetive. I live life by this rule, "There are always two people for a job, you and the other guy.  F--K the other guy."
+    Robert Campagna from Long Island, New York.  Father of twin boys Oliver and Wesley.  Born entertainer with the love for baseball and hockey.  Cancer survivor.  I am competetive. I live life by this rule, "There are always two people for a job, you and the other guy.  F--K the other guy."
   </p>
+  <div className="teams">
+  <p className="mets">Lets Go Mets</p><p className="isles">Lets Go Islanders</p>
+  </div>
   </div>
 </section>
   )
