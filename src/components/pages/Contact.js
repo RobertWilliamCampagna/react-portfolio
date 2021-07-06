5a734526472be425d18bdf47b595ac0f86786c40
Robert Campagna <rwcnyla@gmail.com>
2021-07-06 16:05:34 -0700

    added adaigo element to portfolio

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,19 +1,78 @@
-import React from "react";
+import React, { useState } from 'react';
+import {Form, Button, Card} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.css';
 
 
-// This is the action for the Contact Page
-function Contact() {
+import { validateEmail } from '../utils/helpers';
+
+function ContactForm() {
+  const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+
+  const [errorMessage, setErrorMessage] = useState('');
+  const { name, email, message } = formState;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!errorMessage) {
+      console.log('Submit Form', formState);
+    }
+  };
+
+  const handleChange = (e) => {
+    if (e.target.name === 'email') {
+      const isValid = validateEmail(e.target.value);
+      if (!isValid) {
+        setErrorMessage('Your email is invalid.');
+      } else {
+        setErrorMessage('');
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage('');
+      }
+    }
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+      console.log('Handle Form', formState);
+    }
+  };
+
+
   return (
-  <div className="contact">
-    <h1>Contact Me</h1>
-    <div className="github-image">
-    <a href="https://github.com/RobertWilliamCampagna">
-       <img className="logo" src="https://i.imgur.com/MmJtkOs.png?1" alt="github-logo"/>
-     </a>
-     <p>Link to my GITHUB page.</p>
-    </div>
-  </div>
-  )
+    <section>
+        <h1 class="section-title about">Reach Out:</h1>
+        <div class="contact-container">
+          <Form id="contact-form" onSubmit={handleSubmit}>
+              <div>
+                  <Form.Group controlId="NameInput">
+                      <h3>Name:</h3>
+                      <Form.Control id="nameValue" as="textarea" name="name" defaultValue={name} onBlur={handleChange}/>
+                  </Form.Group>
+
+                  <Form.Group controlId="EmailInput">
+                      <h3>Email:</h3>
+                      <Form.Control id="emailValue" type="email" name="email" defaultValue={email} onBlur={handleChange} />
+                  </Form.Group>
+
+                  <Form.Group controlId="MessageInput">
+                      <h3 htmlFor="message">Message:</h3>
+                      <Form.Control id="messageValue" as="textarea" name="MessageInput" rows={3} defaultValue={message} onBlur={handleChange} />
+                  </Form.Group>
+
+                  {errorMessage && (
+                  <div>
+                      <p className="error-text">{errorMessage}</p>
+                  </div>
+                  )}
+                  <Button data-testid="button" type="submit">Submit</Button>
+              </div>
+          </Form>
+        </div>
+    </section>
+  );
 }
 
-export default Contact
+export default ContactForm;
+
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -4,19 +4,31 @@ import React from "react";
 const Portfolio = () => (
   <div className="portfolio">
     <h1>Portfolio</h1>
-   <div className="port-page">
+    <section className="mycipePage">
+      <div id="mycipeContainer">
      <a href="https://mycipe.herokuapp.com/">
        <img className="chefhat" src="https://i.imgur.com/UhQk31V.png" alt="myCipe app log"/>
      </a>
-     <div>
      <p>
        This is an appilication for chefs, an online database to store and share recipes.  Create a Chef Name. Create a recipe. Share recipe.  Each Chef will have their own Dashboard that will display their own personal recipes.  As a Guest, you may enjoy searching recipes and a Dashbaord with all recipes will be at your disposal.  Enjoy searching recipes!<br></br>
- 
       </p>
-      <footer>Created with my partners<a href="https://github.com/nrhuizar"> Nick Huizar</a>, <a href="https://github.com/ericasiegel">Erica Siegle</a> and <a href="https://github.com/naomiilang">Naomi Lang</a>.</footer>
+      </div>
+    <footer>Created with my partners<a href="https://github.com/nrhuizar"> Nick Huizar</a>, <a href="https://github.com/ericasiegel">Erica Siegle</a> and <a href="https://github.com/naomiilang">Naomi Lang</a>.</footer>
+    </section>
+
+    <section className="adaigoElement">
+      <div id="adaigoContainer">
+      <a href="https://siphon880gh.github.io/your-daily-portal-adaigo-element/">
+        <img className="libra" src="https://i.imgur.com/N7bAPyS.png?1" alt="adaigoElementApp"/>
+      </a>
+      <p>
+        A fun way to start your day!  AdaigoElement is a dashboard that provides your horoscope for the day, along with songs from your favorite artists!
+      </p>
+      </div>
+      <footer>Created by: Robert, Weng and Stephanie.</footer>
+      </section>
      </div>
-   </div>
-  </div>
+
 );
 
 export default Portfolio;
